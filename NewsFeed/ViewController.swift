290b74d31df804d935d27fd5889c204044386605
Denis Ablyzalov <denis.abl@icloud.com>
2020-01-24 11:23:36 +0300

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Денис Аблызалов on 24.01.2020.
+//  Copyright © 2020 Денис Аблызалов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
